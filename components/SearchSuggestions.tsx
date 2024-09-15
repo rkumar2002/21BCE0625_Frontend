5fea14546a00344fb2dbc5581856801a13733024
Rahul Kumar <143000302+rkumar2002@users.noreply.github.com>
2024-09-15 22:13:30 +0530

    commit

--- components/SearchSuggestions.tsx
+++ components/SearchSuggestions.tsx
@@ -27,11 +27,7 @@ const SearchSuggestions: React.FC<SearchSuggestionsProps> = ({ suggestions, onSu
         </div>
       </div>
       <div className="actions flex items-center">
-        <button className="filter-button flex items-center border border-light-gray text-dark-gray px-4 py-2 rounded-lg">
-          <FaFilter className="text-dark-gray mr-2" />
-          Filter
-        </button>
-        <button className="share-button flex items-center justify-center border border-light-gray rounded-full w-10 h-10 ml-4" onClick={onShareClick}>
+        <button className="share-button flex items-center justify-center border border-light-gray rounded-full w-10 h-10 mr-3" onClick={onShareClick}>
           <FaShareAlt className="text-dark-gray" />
         </button>
       </div>
